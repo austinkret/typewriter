d73c769971a420f5d67250e036a0f57847162a8f
austinkret <austinkret@gmail.com>
2021-04-06 19:17:42 +0000

    typewriter

--- typewriter.js
+++ typewriter.js
@@ -1,11 +1,12 @@
 let sentence = "hello there from lighthouse labs";
 
-sentence += '\n';
-
 let delayTime = 0;
 for (const char of sentence) {
   setTimeout(() => {
     process.stdout.write(char);
   }, delayTime);
   delayTime += 50;
+  setTimeout(() => {
+    process.stdout.write('\n');
+  }, sentence.length * 50);
 };
